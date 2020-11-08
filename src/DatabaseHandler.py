1e1aaa23e344d81276b1bf2ad9320f07cf92f522
KOOKIIE <KookiieStudios@outlook.com>
2020-11-09 01:17:59 +0800

    Migrate DB Settings to YAML

--- src/DatabaseHandler.py
+++ src/DatabaseHandler.py
@@ -1,6 +1,7 @@
 import mysql.connector
 
 from src.settings import Config
+from src.Main import DB_SETTINGS
 
 
 class DatabaseHandler:
@@ -11,8 +12,9 @@ class DatabaseHandler:
     @staticmethod
     def get_character_stats(character_name):
         try:
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor(dictionary=True)
             cursor.execute(f"SELECT * FROM characters WHERE name = '{character_name}'")
             rows = cursor.fetchall()
@@ -24,8 +26,9 @@ class DatabaseHandler:
 
     @staticmethod
     def get_character_look(character_id, hair, face, skin):
-        con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                      password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+        # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+        con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                      password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
         cursor = con.cursor(dictionary=True)
         cursor.execute(
             f"SELECT * FROM inventoryitems WHERE characterid = '{character_id}' AND inventorytype = '-1'")  # -1 = equipped
@@ -43,8 +46,9 @@ class DatabaseHandler:
     def get_guild_name(guild_id):
         if guild_id == 0:  # Guildless
             return "None"
-        con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                      password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+        # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+        con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                      password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
         cursor = con.cursor(dictionary=True)
         cursor.execute(
             f"SELECT name FROM guilds WHERE guildid = '{guild_id}'")  # gets the guild name through the guild id
@@ -56,8 +60,9 @@ class DatabaseHandler:
     @staticmethod
     def get_guild_info(name):
         try:
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor(dictionary=True)
             cursor.execute(f"SELECT * FROM guilds WHERE name = '{name}'")
             rows = cursor.fetchall()
@@ -72,8 +77,9 @@ class DatabaseHandler:
         if alliance_id == 0:
             return "None"
         else:
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor(dictionary=True)
             cursor.execute(f"SELECT name FROM alliance WHERE id = '{alliance_id}'")
             rows = cursor.fetchall()
@@ -83,8 +89,9 @@ class DatabaseHandler:
 
     @staticmethod
     def get_character_name(id):
-        con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                      password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+        # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+        con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                      password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
         cursor = con.cursor(dictionary=True)
         cursor.execute(f"SELECT name FROM characters WHERE id = '{id}'")
         rows = cursor.fetchall()
@@ -95,8 +102,9 @@ class DatabaseHandler:
     @staticmethod
     def get_account_id(character_name):
         try:
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor(dictionary=True)
             cursor.execute(f"SELECT accountid from characters WHERE name = '{character_name}'")
             rows = cursor.fetchall()
@@ -115,8 +123,9 @@ class DatabaseHandler:
             id = DatabaseHandler.get_account_id(name)
             if not id:  # checks if id is false which means the character was not found
                 return f"Couldn't find {name}"
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor()
             cursor.execute(f"UPDATE accounts SET banned = -1 WHERE id = '{id}'")
             cursor.execute(f"DELETE FROM macbans WHERE aid = '{id}'")  # aid = account_id
@@ -136,8 +145,9 @@ class DatabaseHandler:
         Return: dict, boolean
         """
         try:
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor(dictionary=True)
             cursor.execute(f"SELECT name, {category}, job FROM characters WHERE gm <= 0 ORDER BY level DESC")
             rows = cursor.fetchall()
@@ -150,8 +160,9 @@ class DatabaseHandler:
     @staticmethod
     def get_vp(id):
         try:
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor(dictionary=True)
             cursor.execute(f"SELECT votepoints from accounts WHERE id = '{id}'")
             rows = cursor.fetchall()
@@ -169,8 +180,9 @@ class DatabaseHandler:
             if not id:
                 return f"Character {name} not found"
             vp = DatabaseHandler.get_vp(id)
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor(dictionary=True)
             total = amount + vp
             cursor.execute(f"UPDATE accounts SET votepoints = {total} where id = '{id}'")
@@ -184,8 +196,9 @@ class DatabaseHandler:
     @staticmethod
     def get_gm_level(name):
         try:
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor(dictionary=True)
             cursor.execute(
                 f"SELECT gm from characters WHERE name = '{name}'")
@@ -207,8 +220,9 @@ class DatabaseHandler:
                 return f"Character {name} not found"
             if lvl == level:
                 return f"{name} is already GM level: {level}"
-            con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER,
-                                          password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            # con = mysql.connector.connect(host=Config.DATABASE_HOST, user=Config.DATABASE_USER, password=Config.DATABASE_PASS, database=Config.DATABASE_NAME)
+            con = mysql.connector.connect(host=DB_SETTINGS['HOST'], user=DB_SETTINGS['NAME'],
+                                          password=DB_SETTINGS['USER'], database=DB_SETTINGS['PASS'])
             cursor = con.cursor(dictionary=True)
             cursor.execute(f"UPDATE characters SET gm = {level} where name = '{name}'")
             # updates gm level, most sources only have it so the characters are gm and not the accounts change this if ur source has account wide gm
