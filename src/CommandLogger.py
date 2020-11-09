4100371f2a89a5ba40a437ccddc030854964e4cb
KOOKIIE <KookiieStudios@outlook.com>
2020-11-09 18:44:24 +0800

    Add comments

--- src/CommandLogger.py
+++ src/CommandLogger.py
@@ -1,5 +1,7 @@
-class Logger:
+# Brandon's Command Logger
+# This is used to track command usages
 
+class Logger:
     @staticmethod
     def log_command(author, command):
         with open("command_logs", "a+") as f:
