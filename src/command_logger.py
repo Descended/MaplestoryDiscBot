bfc712479fd0ecc5ff4bd4170c55512b7ec11a0a
KOOKIIE <KookiieStudios@outlook.com>
2020-11-09 22:16:28 +0800

    Add comment

--- src/command_logger.py
+++ src/command_logger.py
@@ -4,5 +4,6 @@
 class Logger:
     @staticmethod
     def log_command(author, command):
+        # Note: This will give different result when ren from PyCharm vs Batch
         with open("command_logs", "a+") as f:
             f.write(f"{author.name} used command {command}\n")
