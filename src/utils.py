886b22630285017f3f9389151e7313f06b6d046f
KOOKIIE <KookiieStudios@outlook.com>
2020-11-09 21:39:25 +0800

    Add ASCII checker in utils

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,7 @@ class Utils:
 
     @staticmethod
     def get_ordinal_number(num):
-        spirit_logger.debug("Getting ordinal number")
+        # spirit_logger.debug("Getting ordinal number")
         SUFFIXES = {1: 'st', 2: 'nd', 3: 'rd'}
         if 10 <= num % 100 <= 20:
             suffix = 'th'
@@ -22,7 +22,7 @@ class Utils:
     @staticmethod
     def get_job_by_id(id):
         try:
-            spirit_logger.debug("Getting job by ID")
+            # spirit_logger.debug("Getting job by ID")
             with open("settings/jobs.json", "r") as json_file:
                 data = json.load(json_file)
                 job_name = data[str(id)]
@@ -34,7 +34,7 @@ class Utils:
 
     @staticmethod
     def get_guild_logo(guild_mark_id, guild_mark_color_id, guild_background_id, guild_background_color_id):
-        spirit_logger.debug("Getting guild logo")
+        # spirit_logger.debug("Getting guild logo")
         url = f"https://maplestory.io/api/{config.REGION}/{config.VERSION}/GuildMark/background/{guild_background_id}/{guild_background_color_id}/mark/{guild_mark_id}/{guild_mark_color_id}"
         spirit_logger.debug(f"Guild logo: {url}")
         return url
@@ -52,10 +52,22 @@ class Utils:
             cmd: string
             Return: boolean
         """
-        spirit_logger.debug("Checking if command should be logged")
+        # spirit_logger.debug("Checking if command should be logged")
         for command in config.COMMANDS:
             if cmd == config.PREFIX + command:
                 spirit_logger.debug(f"{cmd} should be logged")
                 return True
         spirit_logger.debug(f"{cmd} should not be logged")
         return False
+
+    # Workaround because String.isascii() was only introduced in Python 3.7
+    @staticmethod
+    def is_ascii(string):
+        try:
+            string.encode('ascii')
+        except UnicodeEncodeError:
+            spirit_logger.debug(f"The string {string} is not ASCII")
+            return False
+        else:
+            spirit_logger.debug(f"The string {string} is indeed ASCII")
+            return True
